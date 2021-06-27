54ef448f793dc2a30ab061fc77253f0940d284ac
Neel Adwani <nprosofficial@gmail.com>
2021-06-27 05:07:43 +0000

    Added Hardware Input

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from replit import db
+
+
+
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+
+
+@app.route('/input_hardware')
+def input_hardware():
+  temp = request.args.get('temp')
+  db['temp'] = temp
+  return temp
+
+
 
 app.run(host='0.0.0.0', port=8080)
